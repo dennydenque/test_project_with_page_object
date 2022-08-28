312dc1853f8f7eada344c99eb37a8742493e40d5
Denny Denque <dennydenque@gmail.com>
2022-08-28 14:27:32 +0300

    test(login page): create login page test

---
+++ test_login_page.py
@@ -0,0 +1,9 @@
+from .pages.login_page import LoginPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_login_page()
+
